3c35b98697eb8456edf703cc0ecf52d211225e18
Mike Ruhl <mike.ruhl@tobiidynavox.com>
2021-11-01 11:51:36 -0400

    migrate project from old account

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,21 @@
+﻿using edu_services.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace edu_services.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        private readonly ILogger<ApiController> _logger;
+
+        public ApiController(ILogger<ApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        //TODO: Implement routes & domain using the Classroom object.
+
+    }
+}
